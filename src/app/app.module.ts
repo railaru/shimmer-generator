44f165cdca45b31917e2cce191c754dfc6930127
Rutenis <r.raila@keepthinking.it>
2019-08-19 14:03:20 +0100

    layout updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PreviewComponent } from './preview/preview.component';
+import { CodeOutputComponent } from './code-output/code-output.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviewComponent,
+    CodeOutputComponent,
+    NavbarComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/code-output/code-output.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-output',
+  templateUrl: './code-output.component.html',
+  styleUrls: ['./code-output.component.css']
+})
+export class CodeOutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
